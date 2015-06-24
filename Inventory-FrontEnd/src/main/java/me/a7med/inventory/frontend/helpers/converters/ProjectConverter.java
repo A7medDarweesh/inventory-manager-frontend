f2ebfc7bac427d0477f4aa0e3b98c61f36b50e2f
ahmed_darweeesh <ahmed_darweeesh@192.168.0.55>
2015-06-24 08:21:24 +0200

    Products and product item dialogs and pages

--- Inventory-FrontEnd/src/main/java/me/a7med/inventory/frontend/helpers/converters/ProjectConverter.java
+++ Inventory-FrontEnd/src/main/java/me/a7med/inventory/frontend/helpers/converters/ProjectConverter.java
@@ -1,5 +1,31 @@
 package me.a7med.inventory.frontend.helpers.converters;
 
-public class ProjectConverter {
+import inventory.pl.entities.Project;
+import inventory.pl.entities.User;
+import inventory.pl.services.SearchService;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.inject.Inject;
+@FacesConverter(forClass = Project.class,value = "projectConverter")
+public class ProjectConverter implements Converter{
+    @Inject
+    SearchService searchService;
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+       return searchService.getObjectById(Project.class, Integer.parseInt(value));
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        if(value instanceof Project){
+            Project w=(Project) value;
+            return ""+w.getId();
+        }else{
+            return "no Value";
+        }
+    }
 
 }
